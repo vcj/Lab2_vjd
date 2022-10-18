505d7880910493c0169574c431ad9c826fc32bed
Vicky Johnson <victoria.johnson@gmail.com>
2022-10-18 14:49:41 -0400

    Update main2.js
    forever trying new tiles

--- js/main2.js
+++ js/main2.js
@@ -30,9 +30,8 @@ function createMap(){
     });
 
     //add OSM base tilelayer
-    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
+   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
 var controlLayers = L.control.layers(null, friendly).addTo(map);
